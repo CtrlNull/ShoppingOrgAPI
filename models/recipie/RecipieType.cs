21a43102f79abf1a701d5863dca89bdc3ba2c85d
Ctrl_Null <raphael@fonseca.tech>
2019-04-01 21:33:34 -0400

    added two models

---
+++ models/recipie/RecipieType.cs
@@ -0,0 +1,10 @@
+namespace shoppingAPI.models.RecipieType
+{
+    public class RecipieType
+    {        
+        public int RecType; // TODO: Convert to enum
+        public string RecRegion; // TODO: double check type conventions
+        public string RecDescription;
+        public string RecShortName;
+    }
+}
