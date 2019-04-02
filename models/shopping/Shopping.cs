d3698be826acfab905d89a3a8d76724ade3a9298
Ctrl_Null <raphael@fonseca.tech>
2019-04-01 21:45:51 -0400

    added model

---
+++ models/shopping/Shopping.cs
@@ -0,0 +1,11 @@
+namespace shoppingAPI.models.ShoppingList
+{
+    public class ShoppingList
+    {
+        public int ShopList_ID;
+        public string ShopList_Name;
+        public System.DateTime ShopListDate;
+        public string ShopListItems;
+        public char ShopListNotIncluded;
+    }
+}
